b19f3e09661ef750c0287f268cc647eb3c5b0c59
Ian Norden <iannordenn@gmail.com>
2017-06-02 18:27:14 -0500

    ruby solution for problem 4

--- largpali.rb
+++ largpali.rb
@@ -5,3 +5,43 @@ A palindromic number reads the same both ways. The largest palindrome made from 
 Find the largest palindrome made from the product of two 3-digit numbers.
 
 =end
+
+def palindrome(number1, number2)
+i = 1
+array1 = []
+number1.times do
+  array1 << i
+  i += 1
+end
+j = 1
+array2 = []
+number2.times do
+  array2 << j
+  j += 1
+end
+multiples = []
+array1.each { |x|
+  i = 0
+  number2.times do
+    multiples << x * array2[i]
+    i += 1
+  end
+}
+reverse = []
+palindromes = []
+multiples.each { |x|
+  reverse << x.to_s.reverse.to_i
+}
+x = 0
+reverse.length.times do
+  if reverse[x] == multiples[x]
+  palindromes << reverse[x]
+  end
+  x += 1
+end
+
+largest = palindromes.max
+puts largest
+end
+
+# answer = 906609
