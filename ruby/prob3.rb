a8145a1abd36b6d6f404df023acd926e8d4b9873
Ian Norden <inorden@wisc.edu>
2017-07-06 16:24:45 -0500

    updated problem 3

--- ruby/prob3.rb
+++ ruby/prob3.rb
@@ -7,37 +7,35 @@ What is the largest prime factor of the number 600851475143 ?
 =end
 
 
+
+
 def is_prime?(n)
-  if n == 0
-    return nil
-  end
-  array = []
-  i = 2
-  (n-2).times do
-    array << i
-    i += 1
-  end
-  array.each { |x|
+  return nil if n == 0 || n == 1
+  return true if n == 2
+  return false if n % 2 == 0
+  return true if n == 5
+  return false if n % 5 == 0
+  (2..Math.sqrt(n)).each { |x|
     return false if n % x == 0
   }
   return true
 end
 
 
-
-
 def prime_factor(number)
   primes = []
-  lpfs = []
+  pfs = []
   i = 2
-  (number-2).times do
+  Math.sqrt(number).ceil.times do
     primes << i if is_prime?(i)
     i += 1
   end
   primes.each { |x|
     if number % x == 0
-      lpfs << x
+      pfs << x
     end
   }
-  lpfs
+  pfs[-1]
 end
+
+#answer = 6857
